b3e40d8b215e3848814abfc063ee8ffa23bd3019
pamonyango2019 <pamonyi18@gmail.com>
2019-12-20 00:05:57 +0300

    implement react_budget

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 if (process.env.NODE_ENV === "development") {
   require("dotenv").config({ path: ".env.development" });
+} else if (process.env.NODE_ENV === "production") {
+  require("dotenv").config({ path: ".env.production" });
 }
 
 module.exports = env => {
